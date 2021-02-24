bc742c88d951b379630521c83d8b09f598ab4ca1
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-02-24 20:06:53 +0200

    Add files via upload

---
+++ Hello OPP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_OPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello OPP!");
+        }
+    }
+}
