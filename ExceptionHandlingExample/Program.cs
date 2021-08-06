4636e5adef4a4bb9b5e0577fda07592723bdb948
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-06 13:48:38 +0300

    Add files via upload

---
+++ ExceptionHandlingExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+
+            try
+            {
+                Console.WriteLine("How old are you?");
+                age = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Just enter a number");
+                age = int.Parse(Console.ReadLine());
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Too long number");
+                age = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"{age} are not little");
+        }
+    }
+}
