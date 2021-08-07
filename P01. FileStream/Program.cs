61622ec6832b5b737fb9d38295fbbebcc1ea61a6
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-07 16:44:11 +0300

    Add files via upload

---
+++ P01. FileStream/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace P01._FileStream_Before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File file = new File();
+            file.Sent = 50;
+            file.Length = 200;
+
+            Progress progress = new Progress(file);
+
+            while (true)
+            {
+                file.Sent += 7;
+                Console.WriteLine($"{progress.CurrentPercent()}% sent.");
+                Thread.Sleep(107);
+            }
+
+            Music music = new Music();
+            music.Sent = 10;
+            music.Sent = 312;
+
+            Progress prog = new Progress(music);
+
+            while (true)
+            {
+                music.Sent += 12;
+                Console.WriteLine($"{prog.CurrentPercent()}% sent.");
+                Thread.Sleep(212);
+            }
+        }
+    }
+}
---
+++ P01. FileStream/Progress.cs
@@ -0,0 +1,17 @@
+﻿namespace P01._FileStream_Before
+{
+    public class Progress
+    {
+        private readonly IProgressable progressable;
+
+        public Progress(IProgressable progressable)
+        {
+            this.progressable = progressable;
+        }
+
+        public int CurrentPercent()
+        {
+            return this.progressable.Sent * 100 / this.progressable.Length;
+        }
+    }
+}
