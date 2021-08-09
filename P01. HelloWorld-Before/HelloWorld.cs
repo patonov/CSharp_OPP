ae938001867fb798b7141655cb340e76b527530c
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-08-09 17:27:17 +0300

    Add files via upload

---
+++ P01. HelloWorld-Before/HelloWorld.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace P01._HelloWorld_Before
+{
+    public class HelloWorld
+    {
+        private DateTime time;
+
+        public HelloWorld(DateTime time)
+        {
+            this.Time = time;
+        }
+
+        public HelloWorld()
+        {
+            
+        }
+
+        public DateTime Time
+        {
+            get => this.time;
+
+            set
+            {
+                if (value != null)
+                {
+                    this.time = value;
+                }
+                else
+                {
+                    this.time = DateTime.Now.Hour;
+                }
+            }
+        }
+
+        public string Greeting(string name)
+        {
+            if (this.Time < 12)
+            {
+                return "Good morning, " + name;
+            }
+
+            if (this.Time < 18)
+            {
+                return "Good afternoon, " + name;
+            }
+
+            return "Good evening, " + name;
+        }
+    }
+}
---
+++ P01. HelloWorld-Before/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P01._HelloWorld_Before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.Greeting("Nikolay");
+        }
+    }
+}
