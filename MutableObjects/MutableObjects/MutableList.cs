ceebc54f15c38fb30d95cf75e5196e871fafbe5a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-02-11 23:16:48 +0200

    Add files via upload

---
+++ MutableObjects/MutableObjects/MutableList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MutableObjects
+{
+    public class MutableList
+    {
+        private List<string> studentNames;
+
+        public MutableList(string[] initialNames)
+        {
+            this.studentNames = initialNames.ToList();
+        }
+
+        public IReadOnlyCollection<string> StdentNames
+        {
+            get => this.studentNames.AsReadOnly();
+        }
+            
+    }
+}
---
+++ MutableObjects/MutableObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MutableObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MutableList mutableList = new MutableList(new[] { "Mutko", "Utko", "Putko" });
+
+            foreach (var name in mutableList.StdentNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
