e76d9546e24d1825dcd9a366f259bd9b8543acdc
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-02-12 16:54:02 +0200

    Add files via upload

---
+++ FirstAndReserveTeams/FirstAndReserveTeams/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstAndReserveTeams
+{
+    public class Person
+    {
+       
+        public Person(string firstName, string lastName, int age, double salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public double Salary { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
---
+++ FirstAndReserveTeams/FirstAndReserveTeams/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+namespace FirstAndReserveTeams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Person> persons = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split().ToArray();
+                string name = line[0];
+                string surname = line[1];
+                int age = int.Parse(line[2]);
+                double salary = double.Parse(line[3]);
+                Person person = new Person(name, surname, age, salary);
+                persons.Add(person);
+
+            }
+            
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+            Console.WriteLine(team.ToString());
+        }
+    }
+}
