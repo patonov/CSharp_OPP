83b5e7b7d8c0b642f97e6c0a8618d454c519026d
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-03-07 22:42:31 +0200

    Add files via upload

---
+++ Finally/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hello Exception!");
+            }
+            finally
+            {
+                Console.WriteLine("Despite exception, Hello always gets executed!");
+            }
+        }
+    }
+}
